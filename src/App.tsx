0c5e0b7dd43ac6e127dcce22f03b3e80c8d18aa0
rayhannr <rayhannaufal2017@mail.ugm.ac.id>
2021-02-02 22:25:36 +0700

    add word dictionary

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import { words, shuffleWord } from './constants/words'
+import './tailwind.css'
 
-function App() {
+const App: React.FC = () => {
+  useEffect(() => {
+    const word = shuffleWord(words, 300)
+    console.log(word)
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <p className="text-gray-900 font-bold">Kont</p>
+  )
 }
 
-export default App;
+export default App
