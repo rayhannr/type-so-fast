c32895bf0bdd7955f8280f8954b3b1a18e157cb5
rayhannr <rayhannaufal2017@mail.ugm.ac.id>
2021-02-03 22:52:38 +0700

    add heading

--- src/App.tsx
+++ src/App.tsx
@@ -106,6 +106,12 @@ const App: React.FC = () => {
 
   return (
     <div className="font-inter p-8 md:p-14 lg:p-16">
+      <h1 className="text-center bg-clip-text text-transparent bg-gradient-to-r from-blue-300 to-blue-600 font-bold text-3xl md:text-4xl lg:text-6xl">TypeSoFast!</h1>
+      <h2 className="font-medium text-lg text-center text-gray-500 mt-2">How fast can you type?</h2>
+      <h3 className="text-center text-gray-400 mt-1 mb-5">
+        A simple <a href="https://10fastfingers.com" target="_blank" rel="noreferrer" className="hover:text-gray-500 cursor-pointer">10fastfingers </a>
+         clone for you to test your speed to type in Indonesian
+        </h3>
       <WordContainer words={words} isInputCorrect={isInputCorrect || wordInput.length === 0} />
 
       <div className="flex flex-row items-center justify-center mt-8">
