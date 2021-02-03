7c5a1800f870ff18fdd06f950040822d6e2bed82
rayhannr <rayhannaufal2017@mail.ugm.ac.id>
2021-02-03 14:45:49 +0700

    add more logic to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useMemo } from 'react'
 import { indonesianWords, shuffleWord } from './constants/words'
 import './tailwind.css'
 
@@ -8,6 +8,11 @@ import Input from './components/Input'
 const App: React.FC = () => {
   const [words, setWords] = useState<string[]>([])
   const [wordInput, setWordInput] = useState<string>('')
+  const [isInputCorrect, setIsInputCorrect] = useState<boolean>(true)
+  const [correctKeystroke, setCorrectKeystroke] = useState<number>(0)
+  const [wrongKeystroke, setWrongKeystroke] = useState<number>(0)
+
+  const currentWord = useMemo(() => words[0], [words])
 
   useEffect(() => {
     const shuffledWords = shuffleWord(indonesianWords, 300)
@@ -15,13 +20,44 @@ const App: React.FC = () => {
   }, [])
 
   useEffect(() => {
-    console.log(wordInput)
-  }, [wordInput])
+    console.log(`correct: ${correctKeystroke}`)
+    console.log(`wrong: ${wrongKeystroke}`)
+  }, [wrongKeystroke, correctKeystroke])
+
+  const inputHandler = (inputText: string) => {
+    setWordInput(inputText)
+
+    if (inputText.endsWith(' ')) {
+      setWordInput('')
+    }
+
+    if (inputText.trim().length > 0) {
+      if (inputText.endsWith(' ')) {
+        setWords(prevWords => prevWords.slice(1))
+      }
+
+      if (inputText !== currentWord.slice(0, inputText.length)) {
+        setIsInputCorrect(false)
+      } else {
+        setIsInputCorrect(true)
+      }
+    }
+  }
+
+  const keyUpHandler = (key: string) => {
+    if(key.length === 1 && key !== " "){
+      if(isInputCorrect){
+        setCorrectKeystroke(prev => prev + 1)
+      } else {
+        setWrongKeystroke(prev => prev + 1)
+      }
+    }
+  }
 
   return (
     <div className="font-inter p-8 md:p-14 lg:p-16">
-      <WordContainer words={words} />
-      <Input value={wordInput} onChange={(input) => setWordInput(input)} />
+      <WordContainer words={words} isInputCorrect={wordInput.length === 0 || isInputCorrect} />
+      <Input value={wordInput} onChange={inputHandler} onKeyUp={keyUpHandler} />
     </div>
   )
 }
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,18 +1,20 @@
-import React, { ChangeEvent } from 'react'
+import React, { ChangeEvent, KeyboardEvent } from 'react'
 
 interface Props {
     value: string
     onChange: (inputText: string) => void
+    onKeyUp: (eventKey: string) => void
 }
 
 const Input: React.FC<Props> = props => {
     return (
-        <div className=" w-56 mx-auto">
+        <div className="w-64 sm:w-72 md:w-80 lg:w-96 mx-auto mt-8">
             <input
-                className="w-full mt-4 border border-solid border-blue-400 px-2 py-1 rounded-md focus:outline-none focus:ring-1"
+                className="w-full border border-solid border-blue-400 p-3 rounded-md focus:outline-none focus:ring-1"
                 type="text"
                 value={props.value || ''}
-                onChange={(event: ChangeEvent<HTMLInputElement>) => props.onChange(event.target.value)} />
+                onChange={(event: ChangeEvent<HTMLInputElement>) => props.onChange(event.target.value)}
+                onKeyUp={(event: KeyboardEvent) => props.onKeyUp(event.key)} />
         </div>
     )
 }
